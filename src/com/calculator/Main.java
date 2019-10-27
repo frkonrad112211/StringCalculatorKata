2909da79356d4f5afc3c980b9a19defe3f50811b
frkonrad112211 <fryszkowskikonrad@gmail.com>
2019-10-27 15:49:05 +0100

    Added mvn support

--- src/com/calculator/Main.java
+++
@@ -1,9 +0,0 @@
-package com.calculator;
-
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("String calculator kata by Konrad Fryszkowski");
-        System.out.println("Please look into StringCalculatorTests for test cases.");
-    }
-}
